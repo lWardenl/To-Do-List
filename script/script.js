b9b2558ec60268e8770c1c151e1b002330494717
Warden <samer.charaf3@gmail.com>
2023-11-09 07:18:40 +0200

    Added the edit and delete button functionality

--- script/script.js
+++ script/script.js
@@ -4,10 +4,14 @@ const userInput = document.getElementById("task-input");
 
 let todoList = [];
 addTaskBtn.addEventListener("click", CreateTask);
+userInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") CreateTask();
+});
 
 function CreateTask() {
   if (userInput.value == "") {
     alert("Please write your task before!");
+    return;
   }
   console.log(userInput.value);
   // Creating the item div as a child to todo-list div
@@ -17,17 +21,21 @@ function CreateTask() {
 
   // Creating checkbox element with it's attributes
   const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
+  checkBox.type = "checkbox";
   newTaskContainer.appendChild(checkBox);
 
   // Creating the task text input element
   const taskTextField = document.createElement("input");
-  taskTextField.setAttribute("type", "text");
-  taskTextField.setAttribute("class", "task-text");
-  taskTextField.setAttribute("readonly", "readonly");
+  taskTextField.type = "text";
+  taskTextField.classList.add("task-text");
+  taskTextField.readOnly = true;
   taskTextField.value = userInput.value;
   newTaskContainer.appendChild(taskTextField);
 
+  //Task edit logic
+  taskTextField.addEventListener("blur", function () {
+    taskTextField.readOnly = true;
+  });
   // Creating div that holds the buttons
   const btnContainer = document.createElement("div");
   btnContainer.classList.add("action-btn");
@@ -39,11 +47,21 @@ function CreateTask() {
   editBtn.innerHTML =
     '<img src="./assets/images/pencil.png" alt="edit pencil" />';
   btnContainer.appendChild(editBtn);
+  //Event button logic
+  editBtn.addEventListener("click", function () {
+    taskTextField.readOnly = false;
+  });
 
   // Creating delete button
   const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("edit");
+  deleteBtn.classList.add("delete");
   deleteBtn.innerHTML =
     '<img src="./assets/images/trash.png" alt="delete trash" />';
   btnContainer.appendChild(deleteBtn);
+  // Delete button logic
+  deleteBtn.addEventListener("click", function () {
+    newTaskContainer.remove();
+  });
+
+  userInput.value = "";
 }
